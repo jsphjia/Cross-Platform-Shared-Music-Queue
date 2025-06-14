139b27b2a196d4cc17202a85adee7947e1f5d219
jsphjia <joseph.jia23@gmail.com>
2025-06-14 14:03:23 -0400

    added capability to get next song and entire queue

--- queue-backend/server.js
+++ queue-backend/server.js
@@ -20,22 +20,44 @@ const sessions = {};
 /**
  * Creates a session
  */
-server.post('/session', (req, res)) => {
+server.post('/session', (req, res) => {
   const sessID = Math.random().toString(36).substring(2,8);
   sessions[sessID] = []; // creates a queue for the session
   res.json({sessID});
-}
+});
 
 /**
  * Add a song to the queue
  */
-server.post('/session/:id/track', (req, res)) => {
+server.post('/session/:id/track', (req, res) => {
   const sessID = req.params.id;
   const {title, artist} = req.body;
 
   sessions[sessID].push({title, artist}); // add song to queue
   res.json({message: 'Track ${title} added to session ${sessID}'})
-}
+});
+
+/**
+ * Get the next song in the queue
+ */
+server.get('/session/:id/next', (req, res) => {
+  const sessID = req.params.id;
+  const queue = sessions[sessID];
+  if (!queue || queue.length == 0) {
+    return res.status(404).json(message: 'No tracks in queue');
+  }
+  const next = queue[0];
+  res.json({next});
+});
+
+/**
+ * Gets the entire queue
+ */
+server.get('/session/:id/queue', (req, res) => {
+  const sessID = req.params.id;
+  const queue = sessions[sessID];
+  res.json({queue});
+}) 
 
 server.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
